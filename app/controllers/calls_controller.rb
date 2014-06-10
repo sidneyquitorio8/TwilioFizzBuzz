b1081fd3bbcab755f7f17885f7e8e459c268c542
Sidney <sidneyquitorio8@yahoo.com>
2014-06-10 16:22:39 -0700

    Fix

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -1,7 +1,7 @@
 class CallsController < ApplicationController
 
 	def dial_in
-		render :xml => "<Response><Say>Hello Monkey</Say><Gather numDigits="1" action='/test' method='POST'>
+		render :xml => "<Response><Say>Hello Monkey</Say><Gather numDigits='1' action='/test' method='POST'>
         <Say>To speak to a real monkey, press 1.  Press any other key to start over.</Say>
     </Gather></Response>"
 	end
