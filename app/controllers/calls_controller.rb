8bbccf17bac0fcc5569b87140442b1995da0ca50
Sidney <sidneyquitorio8@yahoo.com>
2014-06-10 16:27:30 -0700

    To-do list

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -1,11 +1,18 @@
 class CallsController < ApplicationController
 
+	# CORRECTLY VALIDATE TWILIO SIGNATURE
+
+
+	# make sure to sanitize input
+	# make sure to handle cases where params are not from twilio
 	def dial_in
 		render :xml => "<Response><Say>Hello Monkey</Say><Gather numDigits='1' action='/test' method='POST'>
         <Say>To speak to a real monkey, press 1.  Press any other key to start over.</Say>
     </Gather></Response>"
 	end
 
+	# make sure to sanitize input
+	# make sure to handle cases where params are not from twilio
 	def test
 		digit = params[:Digits]
 		string = "<Response><Say>"+ digit  +"</Say></Response>"
