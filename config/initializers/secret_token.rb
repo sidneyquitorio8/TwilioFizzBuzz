c7ee89630ba747dea7468e56366b87426e28b0dd
Sidney <sidneyquitorio8@yahoo.com>
2014-06-10 15:01:43 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwilioFizzBuzz::Application.config.secret_token = 'ddd018d705bf9c118bfddf3de177daf0187e57cecff9c4e34f28c4eb1fc10caa9701bdd2b33e1a24c826728f8254f4e76231fab9ed631836f441314b69bf0cd1'
